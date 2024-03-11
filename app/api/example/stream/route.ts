dc24ee524304d451c45ea7b3bf0921016fc1573d
Tomás Cichero <martoxdlol@gmail.com>
2024-03-11 11:41:36 -0300

    stream use nodejs runtime

--- app/api/example/stream/route.ts
+++ app/api/example/stream/route.ts
@@ -2,7 +2,7 @@ import { unstable_noStore } from "next/cache"
 import { NextRequest, NextResponse } from "next/server"
 import { runExample } from "~/lib/run-example"
 
-export const runtime = 'edge'
+export const runtime = 'nodejs'
 
 export async function GET(request: NextRequest) {
     unstable_noStore()
